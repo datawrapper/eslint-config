b3683bc07cd0df0cc6c60219e7af836b37902a52
Simon Jockers <449739+sjockers@users.noreply.github.com>
2021-03-01 14:37:01 +0100

    Add a separate set of rules for chart embeds

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
-## A shared `eslint-config` to be used across Datawrapper projects.
+## A shared set of eslint rules to be used across Datawrapper projects.
 
 To use this config, install it via NPM:
 
 `npm install -D @datawrapper/eslint-config`
 
-And import into your project's `eslint-config`, which most probably lives in its package.json:
+Then you can include it in your project's eslint configuration, which most probably lives in its `package.json`:
 
 ```json
 "eslintConfig": {
     "extends": "@datawrapper/eslint-config"
 }
+```
+
+For chart embeds, there is a particular restrictive set of rules that limits the use of certain `window` and `document` attributes:
+
+```json
+"eslintConfig": {
+    "extends": "@datawrapper/eslint-config/chart-plugin"
+}
+```
+
+This config does not include the necessary parser and plugins for use with Svelte. Here's how the `eslint-config` for a chart plugin would look in practice: 
+
+```json
+"eslintConfig": {
+    "parser": "babel-eslint",
+    "extends": [
+        "@datawrapper/eslint-config/chart-plugin",
+        "plugin:@tivac/svelte/svelte"
+    ],
+    "plugins": [
+        "html",
+        "@tivac/svelte"
+    ]
+},
 ```
--- index.js
+++ index.js
@@ -1,39 +1,5 @@
 module.exports = {
     rules: {
-        "no-restricted-properties": [
-            "error",
-            {
-                "object": "window",
-                "property": "innerHeight",
-                "message": "Please use vis.size() to get the chart embed dimensions."
-            },
-            {
-                "object": "window",
-                "property": "innerWidth",
-                "message": "Please use vis.size() to get the chart embed dimensions."
-            },
-            {
-                "object": "window",
-                "property": "documentElement",
-                "message": "Please use vis.size() to get the chart embed dimensions."
-            },
-            {
-                "object": "document",
-                "property": "querySelector",
-                "message": "Please use [yourRootElement].querySelector instead."
-            },
-            {
-                "object": "document",
-                "property": "querySelectorAll",
-                "message": "Please use [yourRootElement].querySelectorAll instead."
-            }
-        ],
-        "no-restricted-globals": [
-            "warn",
-            "window",
-            "document",
-            "documentElement"
-        ],
         "no-console": [
             "error",
             {
