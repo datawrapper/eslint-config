a090fd28b803f7a72b21851246f11de6f3d8f243
Simon Jockers <449739+sjockers@users.noreply.github.com>
2021-03-01 13:26:12 +0100

    Don't include parser and plugin options

--- index.js
+++ index.js
@@ -1,69 +1,54 @@
 module.exports = {
-    eslintConfig: {
-        parser: "babel-eslint",
-        parserOptions: {
-            ecmaVersion: 6,
-        },
-        rules: {
-            "no-restricted-properties": [
-                "error",
-                {
-                    "object": "window",
-                    "property": "innerHeight",
-                    "message": "Please use vis.size() to get the chart embed dimensions."
-                },
-                {
-                    "object": "window",
-                    "property": "innerWidth",
-                    "message": "Please use vis.size() to get the chart embed dimensions."
-                },
-                {
-                    "object": "window",
-                    "property": "documentElement",
-                    "message": "Please use vis.size() to get the chart embed dimensions."
-                },
-                {
-                    "object": "document",
-                    "property": "querySelector",
-                    "message": "Please use [yourRootElement].querySelector instead."
-                },
-                {
-                    "object": "document",
-                    "property": "querySelectorAll",
-                    "message": "Please use [yourRootElement].querySelectorAll instead."
-                }
-            ],
-            "no-restricted-globals": [
-                "warn",
-                "window",
-                "document",
-                "documentElement"
-            ],
-            "no-console": [
-                "error",
-                {
-                    "allow": [
-                        "warn",
-                        "error"
-                    ]
-                }
-            ],
-            "camelcase": [
-                "warn",
-                {
-                    "ignoreDestructuring": true,
-                    "properties": "never"
-                }
-            ],
-            "@tivac/svelte/property-ordering": "off",
-            "@tivac/svelte/onupdate": "off"
-        },
-        extends: [
-            "plugin:@tivac/svelte/svelte"
+    rules: {
+        "no-restricted-properties": [
+            "error",
+            {
+                "object": "window",
+                "property": "innerHeight",
+                "message": "Please use vis.size() to get the chart embed dimensions."
+            },
+            {
+                "object": "window",
+                "property": "innerWidth",
+                "message": "Please use vis.size() to get the chart embed dimensions."
+            },
+            {
+                "object": "window",
+                "property": "documentElement",
+                "message": "Please use vis.size() to get the chart embed dimensions."
+            },
+            {
+                "object": "document",
+                "property": "querySelector",
+                "message": "Please use [yourRootElement].querySelector instead."
+            },
+            {
+                "object": "document",
+                "property": "querySelectorAll",
+                "message": "Please use [yourRootElement].querySelectorAll instead."
+            }
         ],
-        plugins: [
-            "html",
-            "@tivac/svelte"
-        ]
-    }
+        "no-restricted-globals": [
+            "warn",
+            "window",
+            "document",
+            "documentElement"
+        ],
+        "no-console": [
+            "error",
+            {
+                "allow": [
+                    "warn",
+                    "error"
+                ]
+            }
+        ],
+        "camelcase": [
+            "warn",
+            {
+                "ignoreDestructuring": true,
+                "properties": "never"
+            }
+        ],
+    },
 }
