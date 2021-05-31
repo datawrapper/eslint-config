74b6774590063afcf8690fce4f7c0be476f85172
Gregor Aisch <gka@users.noreply.github.com>
2021-05-31 10:44:34 +0200

    add rules no-unused-vars, prefer-const, and no-prototype-builtins

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 module.exports = {
     rules: {
-        "no-console": [
-            "error",
+        'no-console': [
+            'error',
             {
-                "allow": [
-                    "warn",
-                    "error"
-                ]
+                allow: ['warn', 'error']
             }
         ],
-        "camelcase": [
-            "warn",
+        camelcase: [
+            'warn',
             {
-                "ignoreDestructuring": true,
-                "properties": "never"
+                ignoreDestructuring: true,
+                properties: 'never'
             }
         ],
-    },
-}
+        'no-unused-vars': [
+            'error',
+            {
+                vars: 'all',
+                args: 'after-used',
+                ignoreRestSiblings: false
+            }
+        ],
+        'prefer-const': ['error'],
+        'no-prototype-builtins': ['error']
+    }
+};
